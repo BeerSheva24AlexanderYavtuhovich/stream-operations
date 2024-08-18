1a12ce7d1c62753586ad0ab1ee6a2e391ac6744e
Alexander Yavtuhovich <yavtik@gmail.com>
2024-08-18 13:21:18 +0300

    CW 19 - Streams

---
+++ src/main/java/telran/stream/StreamTasks.java
@@ -0,0 +1,14 @@
+package telran.stream;
+
+public class StreamTasks {
+
+    public static int[] shuffle(int[] arr) {
+        // only one pipeline
+        // without sorting
+        // Arrays.stream()..
+        //return one array of ints with all numbers from a given array but with different order
+        //each method call 
+        return null;
+    }
+
+}
